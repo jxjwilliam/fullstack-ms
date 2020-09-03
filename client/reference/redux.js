9fb3b33f305e227cf9f82c8005cce233c8130b99
william jiang <williamjiang@williams-Air.hitronhub.home>
2020-09-03 12:34:35 -0700

    add more mds, like pattern.md, start graphql from youtube.

---
+++ client/reference/redux.js
@@ -0,0 +1,39 @@
+const combineReducer = (reducers) => {
+    return (state, action) => {
+        return Object.keys(reducers).reduce((nextState, key) => {
+
+            nextState[key] = reducers[key](state[key], action);
+
+            return nextState;
+        }, {});
+    }
+}
+//to call:
+const todoApp = combineReducer({
+    todos,
+    visibilityFilter
+});
+
+
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+    const getState = () => state;
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        };
+    };
+
+    dispatch({});
+    return { getState, dispatch, subscribe };
+};
+
+//to call:
+const store = createStore(counter);
